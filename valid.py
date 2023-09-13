6f4c03eaad9bc882aee84964a9412dbbe3160f3f
AKILA S <143704564+akilaselvi28@users.noreply.github.com>
2023-09-13 15:47:09 +0530

    Add files via upload

---
+++ valid.py
@@ -0,0 +1,20 @@
+Tamil=int(input("enter Tamil marks:"))
+English=int(input("enter English marks:"))
+Commerce=int(input("enter Commerce marks:"))
+Accountancy=int(input("enter Accountancy marks:"))
+Economics=int(input("enter Economics marks:"))
+Computer =int(input("enter Computer marks:"))
+if(Tamil>100):
+    print("Tamil mark is invalid")
+if(English>100):
+    print("English mark is invalid")
+if(Commerce>100):        
+    print("Commerce mark is invalid")
+if(Accountancy>100):
+    print("Accountancy mark is invalid")    
+if(Economics>100):
+    print("Economics mark is invalid")    
+if(Computer>100):
+    print("Computer mark is invalid")  
+
+       
